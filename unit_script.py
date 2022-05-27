d21089b506f78c44ab7af9282a96e1cb93e678e2
lennart.thiesen <lennart.thiesen@stud.uni-goettingen.de>
2022-05-27 16:12:49 +0200

    added unit script example script

--- unit_script.py
+++ unit_script.py
@@ -1,6 +1,7 @@
-# Formats:
-# speed_par: km/h
-# length_par: mm
+# This script contains examples for correct unit conversions in pythons. It
+# should be seen as a template for creating an example script for a
+# python-like unit language.
+
 def calculate_time(velocity_par_kmh, distance_par_mm):
     distance_par_kmh = distance_par_mm / 1000000
     return distance_par_kmh / velocity_par_kmh
@@ -11,4 +12,14 @@ velocity_kmh = velocity_dmh / 10000
 
 print(velocity_kmh)
 
-print(calculate_time(1, 5))
+distance_µm = 200000
+distance_dm = distance_µm / 100000
+
+print(distance_dm)
+
+time_min = 3
+time_sec = time_min * 60
+
+print(time_sec)
+
+print(calculate_time(velocity_kmh, distance_dm))
