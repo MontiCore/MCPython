cbcef42fbce9c1a95626fdba6080809dd5412d59
lennart.thiesen <lennart.thiesen@stud.uni-goettingen.de>
2022-06-27 21:40:00 +0200

    Revert "coco tests"
    This reverts commit 61b348e90a731303d3ba220ae4975314c0e086f1.

--- src/test/java/de/monticore/sipythonunits/SIPythonTest.java
+++ src/test/java/de/monticore/sipythonunits/SIPythonTest.java
@@ -1,17 +1,12 @@
 package de.monticore.sipythonunits;
 
-import de.monticore.io.paths.MCPath;
 import de.monticore.python._ast.ASTPythonScript;
 import de.monticore.sipython._parser.SIPythonParser;
 import de.se_rwth.commons.logging.Log;
-import org.apache.commons.io.FilenameUtils;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Optional;
 
 import static org.junit.Assert.assertTrue;
