a04a6535e15f4bb9f62727873deccfa1f4f9d64e
Markus Blanke <markus.blanke@rwth-aachen.de>
2022-06-30 16:10:59 +0200

    updated GeneratorTest

--- src/test/java/de/monticore/sipython/GeneratorTest.java
+++ src/test/java/de/monticore/sipython/GeneratorTest.java
@@ -3,9 +3,16 @@ package de.monticore.sipython;
 import static org.junit.Assert.assertNotEquals;
 
 import de.monticore.sipython.generator.Generator;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GeneratorTest {
+
+    @Before
+    public void init() {
+        SIPythonMill.init();
+    }
+
     @Test
     public void simpleGeneratorTest() {
         Generator.generate("src/test/resources/python/","simple_python.sipy","target/generate");
