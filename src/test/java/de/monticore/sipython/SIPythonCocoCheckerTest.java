1242d59335625bad4b5c66651e841da6f51ed4b6
lennart.thiesen <lennart.thiesen@rwth-aachen.de>
2022-07-14 11:13:28 +0200

    added tests for common expressions and unit conversions

--- src/test/java/de/monticore/sipython/SIPythonCocoCheckerTest.java
+++ src/test/java/de/monticore/sipython/SIPythonCocoCheckerTest.java
@@ -1,5 +1,6 @@
 package de.monticore.sipython;
 
+import de.monticore.expressions.commonexpressions._cocos.CommonExpressionsASTPlusExpressionCoCo;
 import de.monticore.python._ast.ASTPythonScript;
 import de.monticore.python._cocos.PythonASTFunctionDeclarationCoCo;
 import de.monticore.sipython._cocos.*;
@@ -8,28 +9,17 @@ import de.se_rwth.commons.logging.Log;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
 
 public class SIPythonCocoCheckerTest extends AbstractTest {
 
-	private SIPythonCoCoChecker checker;
-
 	@Before
 	public void init() {
 		Log.init();
 		Log.enableFailQuick(false);
-		SIPythonMill.reset();
-		SIPythonMill.init();
-		SIUnitsMill.initializeSIUnits();
-		checker = new SIPythonCoCoChecker();
-//		 checker.addCoCo(SIPythonSIUnitConversionTypeCheckCoco.getCoCo());
-		checker.addCoCo((PythonASTFunctionDeclarationCoCo) new PythonFunctionDeclarationInStatementBlockCheck());
-		checker.addCoCo(new PythonFunctionParameterDuplicateNameCoco());
-		checker.addCoCo(new PythonFunctionArgumentSizeCoco());
-		checker.addCoCo(new PythonVariableOrFunctionExistsCoco());
-//		 checker.addCoCo((CommonExpressionsASTPlusExpressionCoCo) SIPythonCommonExpressionsTypeCheckCoco.getCoco());
 	}
 
 //	---------------------------------------------------------------
@@ -95,21 +85,37 @@ public class SIPythonCocoCheckerTest extends AbstractTest {
 		);
 	}
 
+	@Test
+	public void checkSIPythonCommonExpressionsTypeCheckCoco() {
+		parseCodeStringAndCheckCoCosAndExpectSuccess(
+				"x = 5 dm/h + 3 km/h"
+		);
+
+		parseCodeStringAndCheckCoCosAndExpectError(
+				"x = 5 dm/h + 3 °C"
+		);
+
+		//checks that all distinct pairs of base units are not allowed in a common expression together
+		performCocoChecksForAllBaseUnits(this::parseCommonExpressionsWithTwoUnitsAndCheckCoCosAndExpectError);
+	}
+
+	@Test
+	public void checkSIPythonSIUnitConversionTypeCheckCoco() {
+		parseCodeStringAndCheckCoCosAndExpectSuccess(
+				"x = dm/h(3 km/h)"
+		);
+
+		parseCodeStringAndCheckCoCosAndExpectError(
+				"x = dm/h(3 °C)"
+		);
+
+		//checks that all distinct pairs of base units are not allowed in a unit conversion expression together
+		performCocoChecksForAllBaseUnits(this::parseSIUnitConversionTypeCheckAndCheckCoCosAndExpectError);
+	}
+
 //	---------------------------------------------------------------
 //	Tests for scripts from files.
 //	---------------------------------------------------------------
-
-	@Test
-	public void parseSimplePython() {
-		String model = "python/simple_python.sipy";
-		parseFromModelFileAndCheckCoCosAndExpectSuccess(model);
-	}
-
-	@Test
-	public void parseUnitScript() {
-		String model = "unit_script.sipy";
-		parseFromModelFileAndCheckCoCosAndExpectSuccess(model);
-	}
 
 	@Test
 	public void parseSimpleSkriptWithFunctionInsideFunctionCoCoError() {
@@ -120,12 +126,6 @@ public class SIPythonCocoCheckerTest extends AbstractTest {
 	@Test
 	public void parsePythonDuplicateFunctionParameterCocoError() {
 		String model = "cocos/pythonDuplicateFunctionParameterCocoError.sipy";
-		parseFromModelFileAndCheckCoCosAndExpectFail(model);
-	}
-
-	@Test
-	public void parsePythonVariableOrFunctionNotExistsCocoError() {
-		String model = "cocos/pythonVariableOrFunctionExistsCocoError.sipy";
 		parseFromModelFileAndCheckCoCosAndExpectFail(model);
 	}
 
@@ -147,12 +147,6 @@ public class SIPythonCocoCheckerTest extends AbstractTest {
 		assertTrue(Log.getErrorCount() > 0);
 	}
 
-	private void parseFromModelFileAndCheckCoCosAndExpectSuccess(String modelFileName) {
-		Optional<ASTPythonScript> astPythonScriptOptional = parseModelFromFileAndReturnASTPythonScript(modelFileName);
-		checkCoCos(astPythonScriptOptional);
-		assertEquals(0, Log.getErrorCount());
-	}
-
 	private void parseFromModelFileAndCheckCoCosAndExpectFail(String modelFileName) {
 		Optional<ASTPythonScript> astPythonScriptOptional = parseModelFromFileAndReturnASTPythonScript(modelFileName);
 		checkCoCos(astPythonScriptOptional);
@@ -160,6 +154,10 @@ public class SIPythonCocoCheckerTest extends AbstractTest {
 	}
 
 	private void checkCoCos(Optional<ASTPythonScript> astPythonScriptOptional) {
+		SIPythonMill.reset();
+		SIPythonMill.init();
+		SIUnitsMill.initializeSIUnits();
+
 		Log.getFindings().clear();
 		if(astPythonScriptOptional.isEmpty()) {
 			fail("Failed to parse the model: The ASTTree is empty!");
@@ -168,13 +166,53 @@ public class SIPythonCocoCheckerTest extends AbstractTest {
 		SIPythonMill.scopesGenitorDelegator().createFromAST(model);
 
 		try {
-			checker.checkAll(model);
+			createSIPythonCoCoChecker().checkAll(model);
 		} catch (Exception e) {
+			e.printStackTrace();
 			fail(e.getMessage());
 		}
+	}
 
-		//this resets the symbol table beside others, to not affect further coco checkings.
-		SIPythonMill.reset();
-		SIPythonMill.init();
+	private SIPythonCoCoChecker createSIPythonCoCoChecker() {
+		SIPythonCoCoChecker siPythonCoCoChecker = new SIPythonCoCoChecker();
+		siPythonCoCoChecker.addCoCo(SIPythonSIUnitConversionTypeCheckCoco.getCoCo());
+		siPythonCoCoChecker.addCoCo((PythonASTFunctionDeclarationCoCo) new PythonFunctionDeclarationInStatementBlockCheck());
+		siPythonCoCoChecker.addCoCo(new PythonFunctionParameterDuplicateNameCoco());
+		siPythonCoCoChecker.addCoCo(new PythonFunctionArgumentSizeCoco());
+		siPythonCoCoChecker.addCoCo(new PythonVariableOrFunctionExistsCoco());
+		siPythonCoCoChecker.addCoCo((CommonExpressionsASTPlusExpressionCoCo) SIPythonCommonExpressionsTypeCheckCoco.getCoco());
+		return siPythonCoCoChecker;
+	}
+
+	/**
+	 * This method executes the given CoCoCheckWithTwoUnits on each possible distinct pair of si base units.
+	 */
+	private void performCocoChecksForAllBaseUnits(CoCoCheckWithTwoUnits coCoCheckWithTwoUnits) {
+		String[] unitArray = {"s", "m", "kg", "A", "K", "mol", "cd"};
+
+		Arrays.stream(unitArray).forEach(unit1 -> Arrays.stream(unitArray).forEach(unit2 -> {
+			if(!unit1.equals(unit2)) {
+				coCoCheckWithTwoUnits.check(unit1, unit2);
+			}
+		}));
+	}
+
+	/**
+	 * This interface allows to specify lambda expressions for coco checking for two si unit strings.
+	 */
+	interface CoCoCheckWithTwoUnits {
+		void check(String unit1, String unit2);
+	}
+
+	private void parseCommonExpressionsWithTwoUnitsAndCheckCoCosAndExpectError(String unit1, String unit2) {
+		parseCodeStringAndCheckCoCosAndExpectError(
+				"x = 5 " + unit1 + " + 3 " + unit2
+		);
+	}
+
+	private void parseSIUnitConversionTypeCheckAndCheckCoCosAndExpectError(String unit1, String unit2) {
+		parseCodeStringAndCheckCoCosAndExpectError(
+				"x = " + unit1 + "( 3 " + unit2 + ")"
+		);
 	}
 }
