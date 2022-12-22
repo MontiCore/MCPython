0eb5d345dbb2dc3177bd1702c1edc5314f41979e
Matthias Nadenau <manadenau@web.de>
2022-12-22 18:48:58 +0100

    Override create functions for PythonParser

--- src/main/java/de/monticore/python/_parser/PythonParser.java
+++ src/main/java/de/monticore/python/_parser/PythonParser.java
@@ -1,10 +1,32 @@
 package de.monticore.python._parser;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class PythonParser extends PythonParserTOP {
+
+    @Override
+    protected PythonAntlrParser create(Reader reader) throws IOException {
+        // Convert reader to String joined by \n
+        var asString = new BufferedReader(reader).lines().collect(Collectors.joining("\n"));
+
+        // Then, convert to C-language style python
+        var cPython = addBracketsToPythonBlocks(asString);
+
+        // Finally, return the original parser
+        return super.create(new StringReader(cPython));
+    }
+
+    @Override
+    protected PythonAntlrParser create(String fileName) throws IOException {
+        return this.create(new FileReader(fileName));
+    }
     
     /**
      * Replaces indentation (w/ spaces, no tabs) based python blocks by C-style bracketed blocks.
