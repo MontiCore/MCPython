be914babea5f3452249c56165e1090f3386c6d72
Matthias Nadenau <manadenau@web.de>
2022-12-22 18:48:04 +0100

    add unit tests for bracket util

---
+++ src/test/java/de/monticore/python/_parser/PythonParserTest.java
@@ -0,0 +1,46 @@
+package de.monticore.python._parser;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import static de.monticore.python._parser.PythonParser.addBracketsToPythonBlocks;
+
+public class PythonParserTest {
+    
+    @Test
+    public void testSingleLineBlock() {
+        String input = "if x > 0:\n  print(x)";
+        String expectedOutput = "if x > 0:{\n  print(x)\n}";
+        assertEquals(expectedOutput, addBracketsToPythonBlocks(input));
+    }
+
+    @Test
+    public void testMultipleLineBlock() {
+        String input = "if x > 0:\n  print(x)\n  print(y)";
+        String expectedOutput = "if x > 0:{\n  print(x)\n  print(y)\n}";
+        assertEquals(expectedOutput, addBracketsToPythonBlocks(input));
+    }
+
+    @Test
+    public void testNestedBlocks() {
+        String input = "if x > 0:\n  if y > 0:\n    print(x)\n    print(y)";
+        String expectedOutput = "if x > 0:{\n  if y > 0:{\n    print(x)\n    print(y)\n  }\n}";
+        assertEquals(expectedOutput, addBracketsToPythonBlocks(input));
+    }
+
+    @Test
+    public void testIndentedBlocks() {
+        String input = "if x > 0:\n  print(x)\n  if y > 0:\n    print(y)";
+        String expectedOutput = "if x > 0:{\n  print(x)\n  if y > 0:{\n    print(y)\n  }\n}";
+        assertEquals(expectedOutput, addBracketsToPythonBlocks(input));
+    }
+
+    @Test
+    public void testMultipleBlocks() {
+        String input = "if x > 0:\n  print(x)\nfor i in range(10):\n  print(i)";
+        String expectedOutput = "if x > 0:{\n  print(x)\n}\nfor i in range(10):{\n  print(i)\n}";
+        assertEquals(expectedOutput, addBracketsToPythonBlocks(input));
+    }
+
+}
