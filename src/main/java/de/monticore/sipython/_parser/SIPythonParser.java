3b02d56aec487fac14a13726d32c23516425a6ad
Matthias Nadenau <manadenau@web.de>
2022-12-23 13:27:24 +0100

    Use brackets in SIPython parser

---
+++ src/main/java/de/monticore/sipython/_parser/SIPythonParser.java
@@ -0,0 +1,31 @@
+package de.monticore.sipython._parser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.stream.Collectors;
+
+import de.monticore.python._parser.PythonParser;
+
+public class SIPythonParser extends SIPythonParserTOP {
+
+    @Override
+    protected SIPythonAntlrParser create(Reader reader) throws IOException {
+        // Convert reader to String joined by \n
+        var asString = new BufferedReader(reader).lines().collect(Collectors.joining("\n"));
+
+        // Then, convert to C-language style python
+        var cPython = PythonParser.addBracketsToPythonBlocks(asString);
+
+        // Finally, return the original parser
+        return super.create(new StringReader(cPython));
+    }
+
+    @Override
+    protected SIPythonAntlrParser create(String fileName) throws IOException {
+        return this.create(new FileReader(fileName));
+    }
+
+}
