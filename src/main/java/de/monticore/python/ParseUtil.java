f9086910ba801a636e7218b324e2130da0916910
Alexander Hellwig <hellwig@se-rwth.de>
2024-01-19 14:18:02 +0100

    Formatting

--- src/main/java/de/monticore/python/ParseUtil.java
+++ src/main/java/de/monticore/python/ParseUtil.java
@@ -13,48 +13,53 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class ParseUtil {
-    public static void main(String[] args) throws IOException {
-        Log.enableFailQuick(false);
-        AtomicInteger overallErrors = new AtomicInteger(0);
-        AtomicInteger fileCounter = new AtomicInteger(0);
-        Files
-                .walk(Paths.get(args[0]))
-                .filter(p -> p.toFile().isFile())
-                .filter(p -> p.toFile().getName().endsWith(".py"))
-                .forEach(p -> {
-                  fileCounter.incrementAndGet();
-                    try {
-                        Log.getFindings().clear();
-                        System.out.println("Parsing file:///" + p.toFile().toString().replace("\\", "/"));
-                      PythonParser parser = PythonMill.parser();
-                      parser.parse(p.toString());
+  public static void main(String[] args) throws IOException {
 
-                      StringBuilder b = new StringBuilder();
+    boolean failAfterFirstErrorFile = true;
+    Log.enableFailQuick(false);
+    AtomicInteger overallErrors = new AtomicInteger(0);
+    AtomicInteger fileCounter = new AtomicInteger(0);
+    Files
+        .walk(Paths.get(args[0]))
+        .filter(p -> p.toFile().isFile())
+        .filter(p -> p.toFile().getName().endsWith(".py"))
+        .forEach(p -> {
+          fileCounter.incrementAndGet();
+          try {
+            Log.getFindings().clear();
+            System.out.println("Parsing file:///" + p.toFile().toString().replace("\\", "/"));
+            PythonParser parser = PythonMill.parser();
+            parser.parse(p.toString());
 
-                      BufferedTokenStream ts = parser.currentTokenStream;
-                      List<String> linebreaks = List.of("⦃", "⁏", "⦄");
-                      for (int i = 0; i < ts.size(); i++){
-                        String text = ts.get(i).getText();
-                        b.append(text);
-                        if(linebreaks.contains(text)){
-                          b.append("\n");
-                        }else{
-                          b.append(" ");
-                        }
-                      }
+            StringBuilder b = new StringBuilder();
 
-                      List<Finding> errors = Log.getFindings().stream().filter(Finding::isError).collect(Collectors.toList());
-                      for (Finding error : errors) {
-                        System.out.println(error);
-                      }
-                      System.out.println(errors.size() + " errors while parsing");
-                      overallErrors.addAndGet(errors.size());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+            BufferedTokenStream ts = parser.currentTokenStream;
+            List<String> linebreaks = List.of("⦃", "⁏", "⦄");
+            for (int i = 0; i < ts.size(); i++) {
+              String text = ts.get(i).getText();
+              b.append(text);
+              if (linebreaks.contains(text)) {
+                b.append("\n");
+              } else {
+                b.append(" ");
+              }
+            }
 
-      System.out.println("Parsed " + fileCounter.intValue() + " file(s) with " + overallErrors.intValue() + " errors");
-      System.out.println((overallErrors.intValue() / (double)fileCounter.intValue()) + " errors per file");
-    }
+            List<Finding> errors = Log.getFindings().stream().filter(Finding::isError).collect(Collectors.toList());
+            for (Finding error : errors) {
+              System.out.println(error);
+            }
+            System.out.println(errors.size() + " errors while parsing");
+            overallErrors.addAndGet(errors.size());
+            if (failAfterFirstErrorFile && !errors.isEmpty()) {
+              throw new RuntimeException("Found errors and failAfterFirstErrorFile is enabled");
+            }
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        });
+
+    System.out.println("Parsed " + fileCounter.intValue() + " file(s) with " + overallErrors.intValue() + " errors");
+    System.out.println((overallErrors.intValue() / (double) fileCounter.intValue()) + " errors per file");
+  }
 }
