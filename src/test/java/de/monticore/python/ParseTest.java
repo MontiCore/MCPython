92a33193df566247953038409b2ecbaeb0627d2d
Alexander Hellwig <hellwig@se-rwth.de>
2025-01-30 13:53:03 +0100

    Additional witness for parsing problems

--- src/test/java/de/monticore/python/ParseTest.java
+++ src/test/java/de/monticore/python/ParseTest.java
@@ -3,11 +3,14 @@ package de.monticore.python;
 import de.monticore.python._ast.ASTPythonScript;
 import de.monticore.python._parser.PythonParser;
 import de.se_rwth.commons.logging.Log;
+import org.antlr.v4.runtime.Token;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.Optional;
 
 public class ParseTest {
@@ -23,6 +26,31 @@ public class ParseTest {
     Assert.assertTrue(file.getAbsolutePath(),file.exists());
     PythonParser p = PythonMill.parser();
     Optional<ASTPythonScript> opt = p.parse(file.getAbsolutePath());
+
+    Log.getFindings().forEach(System.err::println);
+
+    System.err.print("");
+    System.err.print(p.currentTokenStream.getTokens());
+
+    Assert.assertEquals(0, Log.getErrorCount());
+  }
+
+  @Ignore("Error with whitespace processing, use branch preprocessing to solve")
+  @Test
+  public void testTrailingCommaArguments() throws IOException {
+    PythonMill.init();
+    Log.init();
+    Log.enableFailQuick(false);
+
+    PythonParser p = new PythonParser();
+    p.parse(new StringReader("foo.bar(\n" +
+        "    1,\n" +
+        ")"));
+
+    for (Token token : p.currentTokenStream.getTokens()) {
+      System.out.println(token.getText() + " ");
+    }
+
 
     Log.getFindings().forEach(System.err::println);
 
