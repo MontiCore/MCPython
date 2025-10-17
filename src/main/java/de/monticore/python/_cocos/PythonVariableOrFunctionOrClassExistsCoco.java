3812fa36c35e62e1a9a01dd1b7e81acb4bc8fa4f
Alexander Hellwig <hellwig@se-rwth.de>
2025-10-17 13:00:32 +0200

    More cleanup

--- src/main/java/de/monticore/python/_cocos/PythonVariableOrFunctionOrClassExistsCoco.java
+++ src/main/java/de/monticore/python/_cocos/PythonVariableOrFunctionOrClassExistsCoco.java
@@ -2,16 +2,24 @@ package de.monticore.python._cocos;
 
 import de.monticore.expressions.expressionsbasis._ast.ASTNameExpression;
 import de.monticore.expressions.expressionsbasis._cocos.ExpressionsBasisASTNameExpressionCoCo;
-import de.monticore.python._symboltable.PythonScope;
+import de.monticore.expressions.expressionsbasis._symboltable.IExpressionsBasisScope;
+import de.monticore.python._symboltable.IPythonScope;
 import de.se_rwth.commons.logging.Log;
 
 public class PythonVariableOrFunctionOrClassExistsCoco implements ExpressionsBasisASTNameExpressionCoCo {
 
 	@Override
 	public void check(ASTNameExpression node) {
-		if (((PythonScope) node.getEnclosingScope()).resolveVariable(node.getName()).isEmpty()
-				&& ((PythonScope) node.getEnclosingScope()).resolveFunction(node.getName()).isEmpty()
-				&& ((PythonScope) node.getEnclosingScope()).resolvePythonClass(node.getName()).isEmpty()) {
+    IExpressionsBasisScope es = node.getEnclosingScope();
+    if(!(es instanceof IPythonScope)){
+      return;
+    }
+
+    IPythonScope s = (IPythonScope) es;
+
+    if (s.resolveVariable(node.getName()).isEmpty()
+				&& s.resolveFunction(node.getName()).isEmpty()
+				&& s.resolvePythonClass(node.getName()).isEmpty()) {
 			Log.error("Variable, Function or Class " + node.getName() + " does not exist " + node.get_SourcePositionStart());
 		}
 	}
--- src/main/java/de/monticore/python/_symboltable/PythonScopesGenitor.java
+++ src/main/java/de/monticore/python/_symboltable/PythonScopesGenitor.java
@@ -3,7 +3,6 @@ package de.monticore.python._symboltable;
 import de.monticore.expressions.assignmentexpressions._ast.ASTAssignmentExpression;
 import de.monticore.expressions.commonexpressions._ast.ASTFieldAccessExpression;
 import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
-import de.monticore.python.PythonMill;
 import de.monticore.python._ast.*;
 import de.monticore.symbols.basicsymbols._symboltable.VariableSymbol;
 import de.monticore.symbols.oosymbols._symboltable.FieldSymbol;
@@ -11,7 +10,6 @@ import de.monticore.symboltable.ISymbol;
 import de.se_rwth.commons.SourcePosition;
 import de.se_rwth.commons.logging.Log;
 
-import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -65,173 +63,6 @@ public class PythonScopesGenitor extends PythonScopesGenitorTOP {
 				"with",
 				"yield"
 		));
-	}
-
-	private void initPythonKeywordsFromRuntime() {
-		try {
-			File keywordsScript = new File("keywords.py");
-
-			if (!keywordsScript.exists()) {
-				keywordsScript.createNewFile();
-			}
-
-			FileWriter writer = new FileWriter(keywordsScript);
-			writer.write(
-					"import keyword\n" +
-							"\n" +
-							"for k in keyword.kwlist:\n" +
-							"    print(k)\n");
-			writer.close();
-
-			// run the generator script
-			Process process = Runtime.getRuntime().exec("python3 keywords.py");
-
-			BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-			String s;
-
-			while ((s = stdInput.readLine()) != null) {
-				pythonKeywords.add(s);
-			}
-
-			keywordsScript.delete();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-			Log.error("Unable to create keyword list");
-		}
-	}
-
-	public void createBuiltinPythonFunctionsScope() {
-		try {
-			File builtinFunctionsReolverScript = new File("builtin_functions_resolver.py");
-
-			if (!builtinFunctionsReolverScript.exists()) {
-				builtinFunctionsReolverScript.createNewFile();
-			}
-
-			// python script to generate a python script which declares all builtin functions
-			FileWriter writer = new FileWriter(builtinFunctionsReolverScript);
-			writer.write(
-					"for e in __builtins__.__dict__:\n" +
-							"    print(e)\n");
-			writer.close();
-
-			// run the generator script
-			Process process = Runtime.getRuntime().exec("python3 builtin_functions_resolver.py");
-
-			BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-			StringBuilder result = new StringBuilder();
-
-			String s;
-
-			while ((s = stdInput.readLine()) != null) {
-				if (this.pythonKeywords.contains(s)) {
-					continue;
-				}
-				result.append("def ").append(s).append("():\n    null\n");
-			}
-
-			builtinFunctionsReolverScript.delete();
-
-			// parse generated script with builtin function declarations
-			Optional<ASTPythonScript> ast = PythonMill.parser().parse(result.toString());
-
-			if (ast.isEmpty()) {
-				Log.error("Unable to parse builtin python functions");
-				return;
-			}
-
-			// create symbol table
-			IPythonArtifactScope scope = PythonMill.scopesGenitorDelegator().createFromAST(ast.get());
-
-			// set scope name to exclude from parameter size check coco
-			scope.setName("__builtin__");
-
-      PythonMill.globalScope().addSubScope(scope);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-			Log.error("Unable to create builtin python function scope");
-		}
-	}
-
-	public void createImportScopes(ASTPythonScript ast) {
-		try {
-			File functionsResolverScript = new File("import_function_resolver.py");
-
-			if (!functionsResolverScript.exists()) {
-				functionsResolverScript.createNewFile();
-			}
-
-			// create symbol table for each import
-			for (ASTStatement statement : ast.getStatementList()) {
-				if (statement instanceof ASTImportStatement) {
-					String moduleName = ((ASTImportStatement) statement).isPresentModule() ? ((ASTImportStatement) statement).getModule().joined() : null;
-					/*
-					for (ASTPyQualifiedName name : ((ASTImportStatement) statement).getNameList()) {
-						this.createImportScope(functionsResolverScript, moduleName, name.joined());
-					}
-
-					 */
-				}
-			}
-
-			functionsResolverScript.delete();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-			Log.error("Unable to create symbol table for imports");
-		}
-	}
-
-	private void createImportScope(File functionsResolverScript, String moduleName, String name) throws IOException {
-
-		// python script to generate python script, which declares all functions and classes of the imported file
-		FileWriter writer = new FileWriter(functionsResolverScript);
-		writer.write(
-				"from inspect import getmembers, isfunction, signature, isclass\n" +
-						"\n" +
-						(moduleName != null ? ("from " + moduleName + " ") : "") + "import " + name + "\n" +
-						"\n" +
-						"for name, obj in getmembers(" + name + ", isfunction):\n" +
-						"    print(\"def \" + name + str(signature(obj)) + \":\\n    null\\n\")\n" +
-						"for name, obj in getmembers(" + name + ", isclass):\n" +
-						"    print(\"class \" + name + \":\")\n" +
-						"    for name, obj in getmembers(obj, isfunction):\n" +
-						"        print(\"    def \" + name + str(signature(obj)) + \":\\n        null\")\n" +
-						"    else:\n" +
-						"        print(\"    def __init__(self):\\n        null\")\n" +
-						"print()\n");
-		writer.close();
-
-		// run the generator script
-		Process process = Runtime.getRuntime().exec("python3 import_function_resolver.py");
-
-		BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-		StringBuilder result = new StringBuilder();
-
-		String s;
-
-		while ((s = stdInput.readLine()) != null) {
-			result.append(s.replace("'", "\"")).append("\n");
-		}
-
-		// parse generated file
-		Optional<ASTPythonScript> importAst = PythonMill.parser().parse_String(result.toString());
-
-		if (importAst.isEmpty()) {
-			Log.error("Unable to create ast for import " + name);
-			return;
-		}
-
-		// build symbol table of the imported file
-		IPythonArtifactScope scope = PythonMill.scopesGenitorDelegator().createFromAST(importAst.get());
-
-		// add to global scope
-    PythonMill.globalScope().addSubScope(scope);
 	}
 
 	private void checkNameIsKeyword(ISymbol symbol) {
