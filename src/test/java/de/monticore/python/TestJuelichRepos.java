d233b3f591392e0b3f278595748af49225e0009b
Alexander Hellwig <hellwig@se-rwth.de>
2025-10-17 13:44:18 +0200

    Clone projects for parse test instead of having copied files in this repo

--- src/test/java/de/monticore/python/TestJuelichRepos.java
+++ src/test/java/de/monticore/python/TestJuelichRepos.java
@@ -1,17 +1,17 @@
 package de.monticore.python;
 
+import de.monticore.python._ast.ASTPythonScript;
 import de.monticore.python._parser.PythonParser;
 import de.se_rwth.commons.logging.Log;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import de.monticore.python._ast.ASTPythonScript;
-
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class TestJuelichRepos {
 
@@ -25,35 +25,50 @@ public class TestJuelichRepos {
   @Test
   public void testParsingBletlProject() throws IOException {
 
-    File file = new File("src/test/resources/preprocessd_juelich_examples/bletl");
-    for (File testFile : file.listFiles()) {
-      System.out.println("Parsing " + testFile);
-      Assert.assertTrue(testFile.getAbsolutePath(),testFile.exists());
-      PythonParser p = PythonMill.parser();
-      Optional<ASTPythonScript> opt = p.parse(testFile.getAbsolutePath());
+    File file = new File("target/testRepos/bletl");
+    try (var s = Files.walk(file.toPath())) {
+      for (var testPath : s.collect(Collectors.toList())) {
+        File testFile = testPath.toFile();
+        if (!testFile.getName().endsWith(".py")) {
+          continue;
+        } else {
+          System.out.println("Parsing " + testFile);
+        }
 
-      Log.getFindings().forEach(System.err::println);
-      System.out.println(opt);
+        Assert.assertTrue(testFile.getAbsolutePath(), testFile.exists());
+        PythonParser p = PythonMill.parser();
+        Optional<ASTPythonScript> opt = p.parse(testFile.getAbsolutePath());
 
-      System.err.print("");
-      //System.err.print(p.currentTokenStream.getTokens());
-      Assert.assertEquals(0, Log.getErrorCount());
+        Log.getFindings().forEach(System.err::println);
+        System.err.print("");
+        // System.err.print(p.currentTokenStream.getTokens());
+      }
     }
+
+    Assert.assertEquals(0, Log.getErrorCount());
   }
 
   @Test
   public void testParsingCalibr8Project() throws IOException {
 
-    File file = new File("src/test/resources/preprocessd_juelich_examples/calibr8");
-    for (File testFile : file.listFiles()) {
-      Assert.assertTrue(testFile.getAbsolutePath(),testFile.exists());
-      PythonParser p = PythonMill.parser();
-      Optional<ASTPythonScript> opt = p.parse(testFile.getAbsolutePath());
+    File file = new File("target/testRepos/calibr8");
+    try (var s = Files.walk(file.toPath())) {
+      for (var testPath : s.collect(Collectors.toList())) {
+        File testFile = testPath.toFile();
+        if (!testFile.getName().endsWith(".py")) {
+          continue;
+        } else {
+          System.out.println("Parsing " + testFile);
+        }
 
-      Log.getFindings().forEach(System.err::println);
+        Assert.assertTrue(testFile.getAbsolutePath(), testFile.exists());
+        PythonParser p = PythonMill.parser();
+        Optional<ASTPythonScript> opt = p.parse(testFile.getAbsolutePath());
 
-      System.err.print("");
-      //System.err.print(p.currentTokenStream.getTokens());
+        Log.getFindings().forEach(System.err::println);
+        System.err.print("");
+        // System.err.print(p.currentTokenStream.getTokens());
+      }
     }
 
     Assert.assertEquals(0, Log.getErrorCount());
@@ -62,16 +77,24 @@ public class TestJuelichRepos {
   @Test
   public void testParsingMurefiProject() throws IOException {
 
-    File file = new File("src/test/resources/preprocessd_juelich_examples/murefi");
-    for (File testFile : file.listFiles()) {
-      Assert.assertTrue(testFile.getAbsolutePath(),testFile.exists());
-      PythonParser p = PythonMill.parser();
-      Optional<ASTPythonScript> opt = p.parse(testFile.getAbsolutePath());
+    File file = new File("target/testRepos/murefi");
+    try (var s = Files.walk(file.toPath())) {
+      for (var testPath : s.collect(Collectors.toList())) {
+        File testFile = testPath.toFile();
+        if (!testFile.getName().endsWith(".py")) {
+          continue;
+        } else {
+          System.out.println("Parsing " + testFile);
+        }
 
-      Log.getFindings().forEach(System.err::println);
+        Assert.assertTrue(testFile.getAbsolutePath(), testFile.exists());
+        PythonParser p = PythonMill.parser();
+        Optional<ASTPythonScript> opt = p.parse(testFile.getAbsolutePath());
 
-      System.err.print("");
-      //System.err.print(p.currentTokenStream.getTokens());
+        Log.getFindings().forEach(System.err::println);
+        System.err.print("");
+        // System.err.print(p.currentTokenStream.getTokens());
+      }
     }
 
     Assert.assertEquals(0, Log.getErrorCount());
@@ -80,16 +103,24 @@ public class TestJuelichRepos {
   @Test
   public void testParsingRobotoolsProject() throws IOException {
 
-    File file = new File("src/test/resources/preprocessd_juelich_examples/robotools");
-    for (File testFile : file.listFiles()) {
-      Assert.assertTrue(testFile.getAbsolutePath(),testFile.exists());
-      PythonParser p = PythonMill.parser();
-      Optional<ASTPythonScript> opt = p.parse(testFile.getAbsolutePath());
+    File file = new File("target/testRepos/robotools");
+    try (var s = Files.walk(file.toPath())) {
+      for (var testPath : s.collect(Collectors.toList())) {
+        File testFile = testPath.toFile();
+        if (!testFile.getName().endsWith(".py")) {
+          continue;
+        } else {
+          System.out.println("Parsing " + testFile);
+        }
 
-      Log.getFindings().forEach(System.err::println);
+        Assert.assertTrue(testFile.getAbsolutePath(), testFile.exists());
+        PythonParser p = PythonMill.parser();
+        Optional<ASTPythonScript> opt = p.parse(testFile.getAbsolutePath());
 
-      System.err.print("");
-      //System.err.print(p.currentTokenStream.getTokens());
+        Log.getFindings().forEach(System.err::println);
+        System.err.print("");
+        // System.err.print(p.currentTokenStream.getTokens());
+      }
     }
 
     Assert.assertEquals(0, Log.getErrorCount());
