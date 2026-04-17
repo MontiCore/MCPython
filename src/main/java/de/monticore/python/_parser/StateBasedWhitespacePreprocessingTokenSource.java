8c519eff8bcdf027ae421726fc36929f0bde2dd1
Maurice Rosenbaum <rosenbaum.maurice@gmx.de>
2026-04-17 17:33:46 +0200

    Change to preprocessor: Always ignore continueLineTokens

--- src/main/java/de/monticore/python/_parser/StateBasedWhitespacePreprocessingTokenSource.java
+++ src/main/java/de/monticore/python/_parser/StateBasedWhitespacePreprocessingTokenSource.java
@@ -23,33 +23,35 @@ public class StateBasedWhitespacePreprocessingTokenSource {
   private final List<Integer> closingParens;
   private Token lastToken;
   private Token lastEmittedToken;
+  private PreprocessingTokens preprocessingTokens;
 
   public StateBasedWhitespacePreprocessingTokenSource(
-      Pair<TokenSource, CharStream> source,
-      List<Integer> indents,
-      List<Integer> emptyLines,
-      Lexer delegate,
-      Token eolTokenProto,
-      Token incIndentTokenProto,
-      Token decIndentTokenProto,
-      PreprocessingTokens preprocessingTokens
+          Pair<TokenSource, CharStream> source,
+          List<Integer> indents,
+          List<Integer> emptyLines,
+          Lexer delegate,
+          Token eolTokenProto,
+          Token incIndentTokenProto,
+          Token decIndentTokenProto,
+          PreprocessingTokens preprocessingTokens
   ) {
     this.openingParens = List.of(preprocessingTokens.lparenTokenType,
-        preprocessingTokens.lCurlyParenTokenType,
-        preprocessingTokens.lSquareParenTokenType);
+            preprocessingTokens.lCurlyParenTokenType,
+            preprocessingTokens.lSquareParenTokenType);
 
     this.closingParens = List.of(preprocessingTokens.rparenTokenType,
-        preprocessingTokens.rCurlyParenTokenType,
-        preprocessingTokens.rSquareParenTokenType);
+            preprocessingTokens.rCurlyParenTokenType,
+            preprocessingTokens.rSquareParenTokenType);
 
     this.sensitiveProcessor = new WhitespaceSensitiveProcessor(
-        source, indents, emptyLines, delegate.getTokenFactory(),
-        eolTokenProto, incIndentTokenProto, decIndentTokenProto,
-        preprocessingTokens
+            source, indents, emptyLines, delegate.getTokenFactory(),
+            eolTokenProto, incIndentTokenProto, decIndentTokenProto,
+            preprocessingTokens
     );
 
     this.lastToken = null;
     this.lastEmittedToken = null;
+    this.preprocessingTokens = preprocessingTokens;
   }
 
   public List<Token> process(Token token) {
@@ -70,8 +72,12 @@ public class StateBasedWhitespacePreprocessingTokenSource {
       // whitespace sensitive
       res = sensitiveProcessor.process(token, lastToken, lastEmittedToken);
     } else {
-      // whitespace insensitive
-      return List.of(token);
+      // always ignore the continueLineToken
+      if(token.getType() == preprocessingTokens.continueLineTokenType){
+        return List.of();
+      } else {
+        return List.of(token);
+      }
     }
 
     // bookkeeping of emitted tokens
